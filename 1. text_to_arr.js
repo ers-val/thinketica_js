4c08ee66fd5b55edfc47bafd6a12d46fcfed6b88
ers-val <52139306+ers-val@users.noreply.github.com>
2020-10-08 22:08:38 +0300

    added "text to arr" exercise

---
+++ 1. text_to_arr.js
@@ -0,0 +1,25 @@
+function getWordsArray(text) {
+    var newText = text.replace(/[,.\/\\:;'"(){}[\]\!@#=-?]/g, ' ');
+    var wordsArray = newText.split(" ");
+    wordsArray = wordsArray.filter(word => word != false);
+    wordsArray = wordsArray.map(
+        (word) => {
+            return {
+                word: word,
+                length: word.length,
+                isCapitalized: isCapitalized(word)
+            }
+        }
+    );
+    return wordsArray;
+}
+
+function isCapitalized(word) {
+    return Boolean(word.match(/^[A-ZА-Я][a-zа-я]+$/g));
+}
+
+
+/**/
+
+var text = `Когда окончится кризис и начнется ли экономический рост? Почему современный экономический кризис может стать переломным в истории человечества? Поговорим об этом в настоящем ролике.`;
+console.log(getWordsArray(text));
