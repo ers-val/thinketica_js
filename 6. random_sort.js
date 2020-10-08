6e14fe064d0c75820188ff07a7d0278020edd8e3
ers-val <52139306+ers-val@users.noreply.github.com>
2020-10-08 22:11:18 +0300

    Update 6. random_sort.js

--- 6. random_sort.js
+++ 6. random_sort.js
@@ -2,7 +2,6 @@ function randomSort(arr) {
     var newArr = [...arr];
     for (var id = 0; id < arr.length; id++) {
         var rand_id = Math.ceil(Math.random() * arr.length - 1);
-        console.log(`${id} - ${rand_id}`);
         var temp = newArr[rand_id];
         newArr[rand_id] = newArr[id];
         newArr[id] = temp;
